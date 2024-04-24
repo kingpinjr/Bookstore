acb7159b42bba0c8da0540171bf9f53fda3cebb9
BraydenThacker <braydenthacker13@gmail.com>
2024-04-24 13:57:29 -0500

    Authorized AddBook page

--- BookstoreWeb/Pages/Books/AddBook.cshtml.cs
+++ BookstoreWeb/Pages/Books/AddBook.cshtml.cs
@@ -1,5 +1,6 @@
 using BookstoreBusiness;
 using BookstoreWeb.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,6 +10,7 @@ using System.Reflection;
 
 namespace BookstoreWeb.Pages.Books
 {
+    [Authorize]
     [BindProperties]
     public class AddBookModel : PageModel
     {
